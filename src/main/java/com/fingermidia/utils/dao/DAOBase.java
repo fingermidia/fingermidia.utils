cd417b3baf5d6caca19faf709a3a170bf3c89f87
Dirceu Belém <dirceu@fourtime.com>
2022-10-28 14:07:20 -0300

    Criando Data Query

---
+++ src/main/java/com/fingermidia/utils/dao/DAOBase.java
@@ -0,0 +1,423 @@
+
+package com.fingermidia.utils.dao;
+
+import com.fingermidia.utils.DateTime;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author dirceu
+ */
+public class DAOBase {
+
+    private static final List<Object> ALLOW_NULLABLE = Arrays.asList(Timestamp.class, Integer.class, Double.class, Float.class);
+
+    public void insert(Connection c, TOBase t) throws SQLException, Exception {
+
+        boolean temAnterior = false;
+
+        String tabela = Helper.obterNomeTabela(t);
+        List<Field> colunas = Helper.obterColunas(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("insert into ").append(tabela);
+        sql.append("(");
+
+        StringBuilder parm = new StringBuilder();
+        parm.append(") values (");
+
+        for (Field f : colunas) {
+            Object o = Helper.runGetter(f, t);
+
+            if ((o != null || ALLOW_NULLABLE.contains(f.getType())) && !f.getAnnotation(Column.class).jsonData()) {
+                if (temAnterior) {
+                    sql.append(", ");
+                    parm.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                parm.append("?");
+            } else if (f.getAnnotation(Column.class).jsonData()) {
+                if (temAnterior) {
+                    sql.append(", ");
+                    parm.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                parm.append("?::json");
+            }
+        }
+
+        parm.append(")");
+
+        sql.append(parm);
+
+        DataQuery.executeUpdate(c, sql.toString(), valores);
+    }
+
+    public int update(Connection c, TOBase t) throws SQLException, Exception {
+
+        boolean temAnterior = false;
+
+        String tabela = Helper.obterNomeTabela(t);
+        List<Field> colunasSemChave = Helper.obterColunasSemChave(t);
+        List<Field> colunasChave = Helper.obterColunasChave(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("update ").append(tabela).append(" set ");
+
+        for (Field f : colunasSemChave) {
+            Object o = Helper.runGetter(f, t);
+
+            if ((o != null || ALLOW_NULLABLE.contains(f.getType())) && !f.getAnnotation(Column.class).jsonData()) {
+                if (temAnterior) {
+                    sql.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                sql.append(" = ?");
+
+            } else if (f.getAnnotation(Column.class).jsonData()) {
+                if (temAnterior) {
+                    sql.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                sql.append(" = ?::json");
+            }
+        }
+
+        sql.append(DataQuery.WHERE);
+
+        temAnterior = false;
+
+        for (Field f : colunasChave) {
+            Object o = Helper.runGetter(f, t);
+
+            if (o != null) {
+                if (temAnterior) {
+                    sql.append(DataQuery.AND);
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                sql.append(" = ?");
+            }
+        }
+
+        return DataQuery.executeUpdate(c, sql.toString(), valores);
+    }
+
+    public int delete(Connection c, TOBase t) throws SQLException, Exception {
+
+        boolean temAnterior = false;
+
+        String tabela = Helper.obterNomeTabela(t);
+        List<Field> colunasChave = Helper.obterColunasChave(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("delete from ").append(tabela);
+
+        sql.append(DataQuery.WHERE);
+
+        for (Field f : colunasChave) {
+            Object o = Helper.runGetter(f, t);
+
+            if (o != null) {
+                if (temAnterior) {
+                    sql.append(DataQuery.AND);
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                sql.append(" = ?");
+            }
+        }
+
+        return DataQuery.executeUpdate(c, sql.toString(), valores);
+    }
+
+    public JSONObject obtemJSONResultSet(ResultSetData rs, List<Field> colunas) throws Exception {
+        JSONObject j = new JSONObject();
+        for (Field f : colunas) {
+
+            if (f.getAnnotation(Column.class).showJSON()) {
+                String coluna = f.getAnnotation(Column.class).name();
+                String nomeJson = f.getAnnotation(Column.class).jsonName();
+
+                if (f.getAnnotation(Column.class).dateTime()) {
+                    Timestamp ts = rs.getTimestamp(coluna);
+                    DateTime d = new DateTime(ts);
+                    j.put(nomeJson, d.toString(f.getAnnotation(Column.class).dateTimeFormat()));
+                } else if (f.getType() == String.class) {
+                    Object o = rs.getObject(coluna);
+                    if (o != null) {
+                        String v = (String) o;
+                        j.put(nomeJson, v.trim());
+                    }
+                } else {
+                    j.put(nomeJson, rs.getObject(coluna));
+                }
+            }
+
+        }
+        return j;
+    }
+
+    public JSONArray list(Connection c, TOBase t) throws Exception {
+
+        boolean temAnterior = false;
+        List<Object> valores = new ArrayList<>();
+
+        String tabela = Helper.obterNomeTabela(t);
+
+        List<Field> colunas = Helper.obterColunas(t);
+        List<Field> colunasOrdenacao = Helper.obterColunasOrdenacaoPrincipal(t);
+
+        StringBuilder sql = new StringBuilder();
+        sql.append(DataQuery.SELECT);
+
+        for (Field f : colunas) {
+            if (temAnterior) {
+                sql.append(", ");
+            } else {
+                temAnterior = true;
+            }
+            sql.append(f.getAnnotation(Column.class).name());
+        }
+
+        sql.append(DataQuery.FROM).append(tabela);
+
+        sql.append(" order by ");
+
+        temAnterior = false;
+
+        if (colunasOrdenacao.size() > 0) {
+            for (Field f : colunasOrdenacao) {
+                if (temAnterior) {
+                    sql.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+                sql.append(f.getAnnotation(Column.class).name());
+            }
+        } else {
+            sql.append(" 1 ");
+        }
+
+        //System.out.println(sql.toString());
+        JSONArray ja = new JSONArray();
+
+        try (ResultSetData rs = new ResultSetData(c, sql.toString(), valores)) {
+            while (rs.next()) {
+
+                JSONObject j = obtemJSONResultSet(rs, colunas);
+
+                ja.put(j);
+
+            }
+        }
+
+        return ja;
+    }
+
+    public JSONObject list(Connection c, TOBase t, String busca, int reg, int pag) throws Exception {
+        JSONObject jo = new JSONObject();
+
+        boolean temAnterior = false;
+
+        String tabela = Helper.obterNomeTabela(t);
+
+        List<Field> colunas = Helper.obterColunas(t);
+        List<Field> colunasBusca = Helper.obterColunasBusca(t);
+        List<Field> colunasOrdenacao = Helper.obterColunasOrdenacaoPrincipal(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append(DataQuery.SELECT);
+
+        for (Field f : colunas) {
+            if (temAnterior) {
+                sql.append(", ");
+            } else {
+                temAnterior = true;
+            }
+            sql.append(f.getAnnotation(Column.class).name());
+        }
+
+        sql.append(DataQuery.FROM).append(tabela);
+
+        temAnterior = false;
+
+        if (colunasBusca.size() > 0) {
+            sql.append(" where (");
+            for (Field f : colunasBusca) {
+                if (temAnterior) {
+                    sql.append(DataQuery.OR);
+                } else {
+                    temAnterior = true;
+                }
+                sql.append(f.getAnnotation(Column.class).name()).append(" ilike '%' || ? || '%' ");
+                valores.add(busca);
+            }
+            sql.append(" ) ");
+        }
+
+        sql.append(" order by ");
+
+        temAnterior = false;
+
+        if (colunasOrdenacao.size() > 0) {
+            for (Field f : colunasOrdenacao) {
+                if (temAnterior) {
+                    sql.append(", ");
+                } else {
+                    temAnterior = true;
+                }
+                sql.append(f.getAnnotation(Column.class).name());
+            }
+        } else {
+            sql.append(" 1 ");
+        }
+
+        sql.append(" limit ").append(reg).append(" offset ").append(pag);
+
+        JSONArray ja = new JSONArray();
+
+        try (ResultSetData rs = new ResultSetData(c, sql.toString(), valores)) {
+            while (rs.next()) {
+
+                JSONObject j = obtemJSONResultSet(rs, colunas);
+
+                ja.put(j);
+
+            }
+        }
+
+        jo.put("list", ja);
+        jo.put("total", total(c, t, busca));
+        jo.put("page", (pag / reg) + 1);
+        jo.put("registers", reg);
+
+        return jo;
+    }
+
+    public int total(Connection c, TOBase t, String busca) throws Exception {
+
+        List<Field> colunasBusca = Helper.obterColunasBusca(t);
+
+        String tabela = Helper.obterNomeTabela(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("select count(1) as total ");
+        sql.append(DataQuery.FROM).append(tabela);
+
+        boolean temAnterior = false;
+
+        if (colunasBusca.size() > 0) {
+            sql.append(" where (");
+            for (Field f : colunasBusca) {
+                if (temAnterior) {
+                    sql.append(DataQuery.OR);
+                } else {
+                    temAnterior = true;
+                }
+                sql.append(f.getAnnotation(Column.class).name()).append(" ilike '%' || ? || '%' ");
+                valores.add(busca);
+            }
+            sql.append(" ) ");
+        }
+
+        try (ResultSetData rs = new ResultSetData(c, sql.toString(), valores)) {
+
+            if (rs.next()) {
+                return rs.getInt("total");
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public TOBase get(Connection c, TOBase t) throws Exception {
+
+        boolean temAnterior = false;
+
+        String tabela = Helper.obterNomeTabela(t);
+
+        List<Field> colunas = Helper.obterColunas(t);
+        List<Field> colunasChave = Helper.obterColunasChave(t);
+        List<Object> valores = new ArrayList<>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append(DataQuery.SELECT);
+
+        for (Field f : colunas) {
+            if (temAnterior) {
+                sql.append(", ");
+            } else {
+                temAnterior = true;
+            }
+            sql.append(f.getAnnotation(Column.class).name());
+
+        }
+
+        sql.append(DataQuery.FROM).append(tabela);
+        sql.append(DataQuery.WHERE);
+
+        temAnterior = false;
+
+        for (Field f : colunasChave) {
+            Object o = Helper.runGetter(f, t);
+
+            if (o != null) {
+                if (temAnterior) {
+                    sql.append(DataQuery.AND);
+                } else {
+                    temAnterior = true;
+                }
+                valores.add(o);
+                sql.append(f.getAnnotation(Column.class).name());
+                sql.append(" = ?");
+            }
+        }
+
+        try (ResultSetData rs = new ResultSetData(c, sql.toString(), valores)) {
+            if (rs.next()) {
+
+                for (Field f : colunas) {
+                    String coluna = f.getAnnotation(Column.class).name();
+                    Helper.runSetter(coluna, t, rs.getObject(coluna));
+                }
+
+                return t;
+
+            }
+
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/fingermidia/utils/dao/DataQuery.java
@@ -0,0 +1,164 @@
+package com.fingermidia.utils.dao;
+
+import com.fingermidia.utils.DateTime;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DataQuery {
+    public static final String SELECT = " select ";
+    public static final String FROM = " from ";
+    public static final String WHERE = " where ";
+    public static final String AND = " and ";
+    public static final String OR = " or ";
+
+    private static final boolean LOG = false;
+
+    public static Connection openConnectionPostgre(String server, String database, String user, String password) throws Exception {
+        long inicio = System.currentTimeMillis();
+        Connection conn = null;
+        Class.forName("org.postgresql.Driver");
+        conn = DriverManager.getConnection("jdbc:postgresql://" + server + ":5432/" + database, user, password);
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        if (LOG) {
+            System.out.println("Tempo Abrir Conexão: " + tempo);
+        }
+        return conn;
+    }
+
+    public static Connection openConnectionMysql(String server, String database, String user, String password) throws Exception {
+        long inicio = System.currentTimeMillis();
+        Connection conn = null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://" + server + ":3306/" + database, user, password);
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        if (LOG) {
+            System.out.println("Tempo Abrir Conexão: " + tempo);
+        }
+        return conn;
+    }
+
+    public static int executeUpdate(Connection conn, StringBuilder query, Object... parametros) throws SQLException {
+        return executeUpdate(conn, query.toString(), parametros);
+    }
+
+    public static int executeUpdate(Connection conn, String query, Object... parametros) throws SQLException {
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            // Recebe os parâmetros da Query
+            for (int i = 1; i <= parametros.length; i++) {
+                pstmt.setObject(i, parametros[i - 1]);
+            }
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public static int executeUpdate(Connection conn, StringBuilder query, Object p) throws SQLException {
+        return executeUpdate(conn, query.toString(), p);
+    }
+
+    public static int executeUpdate(Connection conn, String query, Object p) throws SQLException {
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setObject(1, p);
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public static int executeUpdate(Connection conn, StringBuilder query, List<Object> p) throws SQLException {
+        return executeUpdate(conn, query.toString(), p);
+    }
+
+    public static int executeUpdate(Connection conn, String query, List<Object> p) throws SQLException {
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            int i = 1;
+            for (Object o : p) {
+                pstmt.setObject(i++, o);
+            }
+            return pstmt.executeUpdate();
+        }
+    }
+
+    public static int executeUpdate(Connection conn, StringBuilder query) throws SQLException {
+        return executeUpdate(conn, query.toString());
+    }
+
+    public static int executeUpdate(Connection conn, String query) throws SQLException {
+        try (Statement stm = conn.createStatement()) {
+            return stm.executeUpdate(query);
+        }
+    }
+
+    public static Object retiraInject(Object o) {
+        if (o != null && o.getClass().getCanonicalName().contains("String")) {
+            String s = (String) o;
+            o = s.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+        }
+        return o;
+    }
+
+    public static void closeResultSet(ResultSet rs) throws Exception {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new Exception("Error closing ResultSet : " + e.getMessage());
+            }
+        }
+    }
+
+    public static List<HashMap<String, Object>> convertResultSetToList(ResultSetData rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        while (rs.next()) {
+            HashMap<String, Object> row = new HashMap<>(columns);
+            for (int i = 1; i <= columns; ++i) {
+                row.put(md.getColumnName(i), rs.getObject(i));
+            }
+            list.add(row);
+        }
+        return list;
+    }
+
+    public static JSONArray convertResultSetToJsonArray(ResultSetData rs) throws SQLException {
+        JSONArray ja = new JSONArray();
+        while (rs.next()) {
+            ja.put(convertResultSetToJsonObjectWithoutNext(rs));
+        }
+        return ja;
+    }
+
+    public static JSONObject convertResultSetToJsonObject(ResultSetData rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        if (rs.next()) {
+            return convertResultSetToJsonObjectWithoutNext(md, rs);
+        } else {
+            return null;
+        }
+    }
+
+    public static JSONObject convertResultSetToJsonObjectWithoutNext(ResultSetData rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        return convertResultSetToJsonObjectWithoutNext(md, rs);
+    }
+
+    public static JSONObject convertResultSetToJsonObjectWithoutNext(ResultSetMetaData md, ResultSetData rs) throws SQLException {
+        JSONObject jo = new JSONObject();
+        for (int i = 1; i <= md.getColumnCount(); ++i) {
+            Object o = rs.getObject(i);
+            if (o.getClass() == Timestamp.class) {
+                DateTime d = new DateTime((Timestamp) o);
+                jo.put(md.getColumnName(i), d.toString("dd/MM/yyyy HH:mm:ss"));
+            } else {
+                jo.put(md.getColumnName(i), o);
+            }
+        }
+        return jo;
+    }
+
+}
