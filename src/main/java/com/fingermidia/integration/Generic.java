e34d4b2934345194e151363175fd552f7c61f59d
Dirceu Belém <dirceubelem@gmail.com>
2023-09-04 11:04:47 -0300

    Adicionando RequestPost SSL

--- src/main/java/com/fingermidia/integration/Generic.java
+++ src/main/java/com/fingermidia/integration/Generic.java
@@ -2,15 +2,71 @@ package com.fingermidia.integration;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
 
 public class Generic {
+
+    public static Response requestPostSSL(Request req) throws Exception {
+
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        InputStream keyStoreInput = new FileInputStream(req.getCertificatePath());
+        keyStore.load(keyStoreInput, req.getCertificatePassword().toCharArray());
+
+        SSLContext sslContext = SSLContexts.custom()
+                .loadKeyMaterial(keyStore, req.getCertificatePassword().toCharArray())
+                .useTLS()
+                .build();
+
+        HttpClientBuilder builder = HttpClientBuilder.create();
+        SSLConnectionSocketFactory sslConnectionFactory = new SSLConnectionSocketFactory(
+                sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        builder.setSSLSocketFactory(sslConnectionFactory);
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", sslConnectionFactory)
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+        HttpClientConnectionManager ccm = new BasicHttpClientConnectionManager(registry);
+        builder.setConnectionManager(ccm);
+
+        // Perform a sample HTTP request.
+        try (CloseableHttpClient httpClient = builder.build()) {
+
+            HttpPost r = new HttpPost(req.getUrl());
+            req.getHeaders().entrySet().forEach((entry) -> {
+                r.addHeader(entry.getKey(), entry.getValue());
+            });
+            r.setEntity(new StringEntity(req.getBody()));
+
+            try (CloseableHttpResponse response = httpClient.execute(r)) {
+                HttpEntity entity = response.getEntity();
+                Response res = getResponse(response);
+                EntityUtils.consume(entity);
+                return res;
+            }
+
+        }
+    }
 
     public static Response requestPost(Request req) throws Exception {
 
--- src/main/java/com/fingermidia/integration/Request.java
+++ src/main/java/com/fingermidia/integration/Request.java
@@ -6,12 +6,13 @@
 package com.fingermidia.integration;
 
 /**
- *
  * @author dirceubelem
  */
 public class Request extends Data {
 
     private String url;
+    private String certificatePath;
+    private String certificatePassword;
 
     public Request() {
         super();
@@ -26,4 +27,19 @@ public class Request extends Data {
         this.url = url;
     }
 
+    public String getCertificatePath() {
+        return certificatePath;
+    }
+
+    public void setCertificatePath(String certificatePath) {
+        this.certificatePath = certificatePath;
+    }
+
+    public String getCertificatePassword() {
+        return certificatePassword;
+    }
+
+    public void setCertificatePassword(String certificatePassword) {
+        this.certificatePassword = certificatePassword;
+    }
 }
